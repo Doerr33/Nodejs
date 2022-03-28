7f8653b8b1c53e4ba07f6d6c79a7368bec0fcef6
Doerr <oncwnuPKFDjxU5s5u3w94XFNBm9A@git.weixin.qq.com>
2022-03-28 15:25:21 +0800

    课程所授的所有接口均已完成，需要补齐几个接口

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const cors = require('cors')
 // 将 cors 注册为全局中间件
 app.use(cors())
 
+// 托管静态资源文件
+app.use('/uploads', express.static('./uploads'))
+
+
 // 配置解析 application/x-www-form-urlencoded 格式的表单数据的中间件：
 app.use(express.urlencoded({ extended: false }))
 
@@ -49,6 +53,19 @@ app.use('/api', userRouter)
 const userinfoRouter = require('./router/userinfo')
 // 注意：以 /my 开头的接口，都是有权限的接口，需要进行 Token 身份认证
 app.use('/my', userinfoRouter)
+
+
+// 导入并使用文章分类路由模块
+const artCateRouter = require('./router/artcate')
+// 为文章分类的路由挂载统一的访问前缀 /my/article
+app.use('/my/article', artCateRouter)
+
+
+// 导入并使用文章路由模块
+const articleRouter = require('./router/article')
+// 为文章的路由挂载统一的访问前缀 /my/article
+app.use('/my/article', articleRouter)
+
 
 
 // 路由之后定义错误中间件
--- router/user.js
+++ router/user.js
@@ -9,6 +9,9 @@ const expressJoi = require('@escook/express-joi')
 // 2. 导入需要的验证规则对象
 const { reg_login_schema } = require('../schema/user')
 
+
+
+
 // 注册新用户
 // 3. 在注册新用户的路由中，声明局部中间件，对当前请求中携带的数据进行验证
 // 3.1 数据验证通过后，会把这次请求流转给后面的路由处理函数
@@ -16,5 +19,7 @@ const { reg_login_schema } = require('../schema/user')
 router.post('/reguser', expressJoi(reg_login_schema), userHandler.regUser)
 // 登录
 router.post('/login', expressJoi(reg_login_schema), userHandler.login)
+
+
 
 module.exports = router
